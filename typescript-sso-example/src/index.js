d4577d7290c9d5a5f0c039d901f63d24da18b12b
inertiawhoosh <victorialgodfrey@gmail.com>
2022-07-12 16:55:50 -0600

    add basic styling to sso example

--- typescript-sso-example/src/index.js
+++ typescript-sso-example/src/index.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var router = require('./routes/index');
 var path = require('path');
-
-const app = express();
-const port = 3000;
-
+var app = express();
+var port = 3000;
 app.use("/", router);
 app.use(express.static(path.join(__dirname)));
-
-
 // start the Express server
-app.listen( port, () => {
-  // tslint:disable-next-line:no-console
-  console.log( `server started at ${port}` );
-} );
+app.listen(port, function () {
+    // tslint:disable-next-line:no-console
+    console.log("server started at ".concat(port));
+});
--- typescript-sso-example/src/routes/index.js
+++ typescript-sso-example/src/routes/index.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
-
-
-router.get('/', (req, res) => {
+router.get('/', function (req, res) {
     res.render('index.ejs', { title: 'Express' });
 });
-
 module.exports = router;
