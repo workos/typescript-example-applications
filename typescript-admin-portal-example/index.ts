a4ff8112de576fb3fdb4791fd8af863f9aa336e9
inertiawhoosh <victorialgodfrey@gmail.com>
2022-07-15 13:28:24 -0600

    basic functioning admin portal example

--- typescript-admin-portal-example/index.ts
+++ typescript-admin-portal-example/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import router from "./routes/index";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use("/", router);
+app.use('/public', express.static('public'));
 
 // start the Express server
 app.listen( port, () => {
---
+++ typescript-admin-portal-example/routes/index.ts
@@ -0,0 +1,81 @@
+import express from "express";
+const router = express.Router();
+import WorkOS, { GeneratePortalLinkIntent } from '@workos-inc/node';
+import { Organization } from "@workos-inc/node/lib/organizations/interfaces/organization.interface";
+
+const workos = new WorkOS("");
+
+var organization: Organization = {
+    object: 'organization',
+    id: "",
+    name: "",
+    allow_profiles_outside_organization: false,
+    domains: [],
+    created_at: "",
+    updated_at: "",
+}
+
+router.get('/', (req, res) => {
+    res.render("index.ejs", {
+        title: "Home",
+    });
+});
+
+router.post('/provision-enterprise', async (req, res) => {
+    const organizationName =  req.body.org
+    const domains = req.body.domain.split(" ")
+    
+    // Make call to listOrganizations and filter using the domain passed in by user.
+    const organizations = await workos.organizations.listOrganizations({
+      domains: domains,
+    });
+    // If no organizations exist with that domain, create one.
+    if (organizations.data.length === 0) {
+      organization = await workos.organizations.createOrganization({
+        name: organizationName,
+        domains: domains,
+      });
+      res.render('logged_in.ejs');
+    }
+    // If an organization does exist with the domain, use that organization for the connection.
+    else {
+      organization = organizations.data[0];
+      res.render('logged_in.ejs');
+    }
+  });
+
+  router.get('/sso-admin-portal', async (_req, res) => {
+    const organizationID =  organization.id;
+  
+    try {
+        // Generate an SSO Adnim Portal Link using the Organization ID from above.
+        const { link } = await workos.portal.generateLink({
+        organization: organizationID,
+        intent: GeneratePortalLinkIntent.SSO,
+        });
+    
+        res.redirect(link);
+    } catch (error){
+        console.log(error)
+    }
+  });
+  
+  router.get('/dsync-admin-portal', async (_req, res) => {
+    const organizationID =  organization.id;
+  
+    try {
+        // Generate an SSO Adnim Portal Link using the Organization ID from above.
+        const { link } = await workos.portal.generateLink({
+        organization: organizationID,
+        intent: GeneratePortalLinkIntent.DSync,
+        });
+    
+        res.redirect(link);
+    }
+    catch (error) {
+        console.log(error)
+    }
+  });
+  
+
+export default router;
