cff66bde3669ddcff60743d32ebe3a0dd9ad27fc
Edita Ignot <edignot@gmail.com>
2023-01-27 21:58:05 -0700

    remove semi colons

--- typescript-directory-sync-example/index.ts
+++ typescript-directory-sync-example/index.ts
@@ -4,13 +4,13 @@ import { Server, Socket } from 'socket.io'
 import morgan from 'morgan'
 import 'dotenv/config'
 
-const app: Application = express();
+const app: Application = express()
 const port: string = process.env.PORT || '8000'
 const workos: WorkOS = new WorkOS(process.env.WORKOS_API_KEY)
 
 app.use('/public', express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(morgan('dev'))
 
@@ -23,38 +23,38 @@ const server = app.listen(port, (): void => {
 const io: Server = new Server(server)
 
 io.on('connection', (socket: Socket) => {
-    console.log('connected');
+    console.log('connected')
 
     socket.on('disconnect', () => {
         console.log('disconnected')
     })
 })
 
-app.get("/", async (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
     const directories = await workos.directorySync.listDirectories()
 
-    res.render("index", {
-        title: "Home",
+    res.render('index', {
+        title: 'Home',
         directories: directories.data
     })
 })
 
 app.get('/directory/:id', async (req: Request, res: Response) => {
-    const directories = await workos.directorySync.listDirectories();
+    const directories = await workos.directorySync.listDirectories()
     const directory: Directory = await directories.data.filter((directory: Directory) => {
         return directory.id == req.params.id
     })[0]
     res.render('directory.ejs', {
         directory: directory,
-        title: "Directory"
+        title: 'Directory'
     })
 })
 
 app.post('/webhooks', async (req: Request, res: Response) => {
     const webhook = workos.webhooks.constructEvent({
         payload: req.body,
-        sigHeader: typeof req.headers['workos-signature'] === 'string' ? req.headers['workos-signature'] : "",
-        secret: process.env.WORKOS_WEBHOOK_SECRET !== undefined ? process.env.WORKOS_WEBHOOK_SECRET : "",
+        sigHeader: typeof req.headers['workos-signature'] === 'string' ? req.headers['workos-signature'] : '',
+        secret: process.env.WORKOS_WEBHOOK_SECRET !== undefined ? process.env.WORKOS_WEBHOOK_SECRET : '',
         tolerance: 90000,
     })
     io.emit('webhook event', { webhook })
@@ -64,63 +64,63 @@ app.post('/webhooks', async (req: Request, res: Response) => {
 
 app.get('/webhooks', async (req, res) => {
     res.render('webhooks.ejs', {
-        title: "Webhooks"
-    });
-});
+        title: 'Webhooks'
+    })
+})
 
 app.get('/directory/:id/usersgroups', async (req: Request, res: Response) => {
-    const directories = await workos.directorySync.listDirectories();
+    const directories = await workos.directorySync.listDirectories()
     const directory = await directories.data.filter((directory: Directory) => {
         return directory.id == req.params.id
     })[0]
     const groups = await workos.directorySync.listGroups({
         directory: req.params.id,
-    });
+    })
     const users = await workos.directorySync.listUsers({
         directory: req.params.id,
-    });
+    })
 
     res.render('groups.ejs', {
         groups: groups.data,
         directory: directory,
         users: users.data,
-        title: "Group & Users"
+        title: 'Group & Users'
     })
 })
 
 app.get('/directory/:id/group/:groupId', async (req: Request, res: Response) => {
-    const directories = await workos.directorySync.listDirectories();
+    const directories = await workos.directorySync.listDirectories()
     const directory = await directories.data.filter((directory: Directory) => {
         return directory.id == req.params.id
     })[0]
     const groups = await workos.directorySync.listGroups({
         directory: req.params.id,
-    });
+    })
     const group = await groups.data.filter((group: Group) => {
         return group.id == req.params.groupId
     })[0]
     res.render('group.ejs', {
         directory: directory,
-        title: "Directory",
+        title: 'Directory',
         group: JSON.stringify(group, null, 2),
         rawGroup: group
     })
 })
 
 app.get('/directory/:id/user/:userId', async (req: Request, res: Response) => {
-    const directories = await workos.directorySync.listDirectories();
+    const directories = await workos.directorySync.listDirectories()
     const directory = await directories.data.filter((directory: Directory) => {
         return directory.id == req.params.id
     })[0]
     const users = await workos.directorySync.listUsers({
         directory: req.params.id,
-    });
+    })
     const user = await users.data.filter((user: User) => {
         return user.id == req.params.userId
     })[0]
     res.render('user', {
         directory: directory,
-        title: "Directory",
+        title: 'Directory',
         user: JSON.stringify(user, null, 2),
         rawUser: user
     })
