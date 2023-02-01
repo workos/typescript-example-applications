6628c0018f636da878ca1441b938de9668081328
Edita Ignot <edignot@gmail.com>
2023-02-01 16:04:32 -0700

    extend session data

---
+++ typescript-sso-example/types/session.d.ts
@@ -0,0 +1,12 @@
+
+import { Profile } from "@workos-inc/node/lib/sso/interfaces/profile.interface"
+
+declare module "express-session" {
+    export interface SessionData {
+        isloggedin?: boolean;
+        profile: Profile;
+        first_name: string;
+    }
+}
+
+export{}
