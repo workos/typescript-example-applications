7608aa2875d46e01fb9c6f5b19b649241908a470
Edita Ignot <edignot@gmail.com>
2023-05-15 15:13:45 -0600

    Admin Portal app UI update

--- typescript-admin-portal-example/routes/index.ts
+++ typescript-admin-portal-example/routes/index.ts
@@ -10,62 +10,55 @@ const router: Router = express.Router()
 let organization: Organization
 
 router.get('/', (req: Request, res: Response) => {
-  res.render('index.ejs', {
-    title: 'Home',
-  })
-})
-
-router.post('/provision-enterprise', async (req: Request, res: Response) => {
-  const organizationName: string = req.body.org ? req.body.org : ''
-  const domains: string[] = req.body.domain ? req.body.domain.split(' ') : []
-  const organizations: List<Organization> = await workos.organizations.listOrganizations({
-    domains: domains,
-  })
-
-  if (organizations.data.length === 0) {
-    organization = await workos.organizations.createOrganization({
-      name: organizationName,
-      domains: domains,
+    res.render('index.ejs', {
+        title: 'Home',
     })
-
-    res.render('logged_in.ejs')
-  } else {
-    organization = organizations.data[0]
-
-    res.render('logged_in.ejs')
-  }
 })
 
-router.get('/sso-admin-portal', async (req: Request, res: Response) => {
-  const organizationID: string = organization.id
+router.post('/provision_enterprise', async (req: Request, res: Response) => {
+    const organizationName: string = req.body.org ? req.body.org : ''
+    const domains: string[] = req.body.domain ? req.body.domain.split(' ') : []
+    const organizations: List<Organization> =
+        await workos.organizations.listOrganizations({
+            domains: domains,
+        })
 
-  try {
+    if (organizations.data.length === 0) {
+        organization = await workos.organizations.createOrganization({
+            name: organizationName,
+            domains: domains,
+        })
+
+        res.render('logged_in.ejs')
+    } else {
+        organization = organizations.data[0]
+
+        res.render('logged_in.ejs')
+    }
+})
+
+router.get('/launch_admin_portal', async (req, res) => {
+    const organizationID: string = organization.id
+
+    let intent: GeneratePortalLinkIntent
+
+    if (
+        typeof req.query.intent === 'string' &&
+        Object.values(GeneratePortalLinkIntent)
+            .map(String)
+            .includes(req.query.intent)
+    ) {
+        intent = req.query.intent as GeneratePortalLinkIntent
+    } else {
+        intent = GeneratePortalLinkIntent.SSO
+    }
+
     const { link }: { link: string } = await workos.portal.generateLink({
-      organization: organizationID,
-      intent: GeneratePortalLinkIntent.SSO,
+        organization: organizationID,
+        intent: intent,
     })
 
     res.redirect(link)
-  } catch (error) {
-    console.log(error)
-  }
 })
-
-router.get('/dsync-admin-portal', async (_req: Request, res: Response) => {
-  const organizationID: string = organization.id
-
-  try {
-    const { link }: { link: string } = await workos.portal.generateLink({
-      organization: organizationID,
-      intent: GeneratePortalLinkIntent.DSync,
-    })
-
-    res.redirect(link)
-  }
-  catch (error) {
-    console.log(error)
-  }
-})
-
 
 export default router
